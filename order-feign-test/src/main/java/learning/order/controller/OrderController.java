6c3ac17bbe05e776f69ff0794b1477ea8701bb20
PYB <puyb@tibetjc.com.cn>
2023-01-05 20:00:05 +0800

    111111

--- order-feign-test/src/main/java/learning/order/controller/OrderController.java
+++ order-feign-test/src/main/java/learning/order/controller/OrderController.java
@@ -1,15 +1,16 @@
 package learning.order.controller;
 
 import com.alibaba.fastjson.JSON;
+import learning.order.BigdecimalTest;
 import learning.order.generator.domain.SysUser;
 import learning.order.generator.service.SysUserService;
 import learning.order.aspect.AnnotationLogAspect;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -20,8 +21,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/order")
 public class OrderController {
-    @Autowired
-    RestTemplate restTemplate;
+
 //    @Autowired
 //    ScockFeignService scockFeignService;
 //    @RequestMapping("/add")
@@ -34,6 +34,8 @@ public class OrderController {
 
     @Resource
     private SysUserService sysUserService;
+    @Resource
+    private RedisTemplate redisTemplate;
     @RequestMapping("/addOrderTest")
     @AnnotationLogAspect(description = "测试",value = "addOrderTest")
     public String addOrderTest(){
@@ -41,10 +43,22 @@ public class OrderController {
         Object o = JSON.toJSON(list);
         String s = JSON.toJSONString(list);
         List<SysUser> sysUsers = JSON.parseArray(s, SysUser.class);
+        redisTemplate.opsForValue().set("sysUsers", sysUsers);
+        List<SysUser> sysUsers1 = (List<SysUser>) redisTemplate.opsForValue().get("sysUsers");
+        SysUser sysUser = sysUsers1.get(0);
         System.out.println("下单");
-        System.out.println("退费来源为移机工单的确认选择必须为”是“");
 //        String reduct = scockFeignService.reduct();
 //        System.out.println(reduct);
         return "下单";
+    }
+    @RequestMapping("/test2")
+    @AnnotationLogAspect(description = "测试",value = "addOrderTest")
+    public BigdecimalTest test2(){
+        BigdecimalTest bigdecimalTest = new BigdecimalTest();
+        BigDecimal bigDecimal = new BigDecimal("3");
+        bigdecimalTest.setVar1(bigDecimal);
+        bigdecimalTest.setVar2(new BigDecimal("0"));
+        bigdecimalTest.setVar3(new BigDecimal("45645.4565648945"));
+        return bigdecimalTest;
     }
 }

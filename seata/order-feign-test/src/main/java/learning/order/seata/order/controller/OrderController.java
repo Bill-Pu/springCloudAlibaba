a8cfa1eb34b60a089c4d747549a2e4d8c12cac6a
PYB <puyb@tibetjc.com.cn>
2023-01-11 11:52:55 +0800

    dev change

--- seata/order-feign-test/src/main/java/learning/order/seata/order/controller/OrderController.java
+++ seata/order-feign-test/src/main/java/learning/order/seata/order/controller/OrderController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.math.BigDecimal;
 import java.util.List;
 
 /**
